3afeefeca63fb23dac419cd6cfce1dc07e349d32
William Richardson <williegroovy@yahoo.com>
2015-12-25 10:05:35 -0600

    Working on footer sliding in.

--- js/custom.js
+++ js/custom.js
@@ -4,32 +4,33 @@ $(document).ready(function() {
     var active = false;
 
     $(window).scroll(function() {
-        console.log($window.scrollTop());
+        console.log("Scroll: " + $window.scrollTop());
+        console.log("Window Size: " + $window.height());
         if($window.scrollTop() > 0) {
             console.log(active);
-            if($window.scrollTop() > 100) {
-                console.log("> 100");
-                $('#footer').css('transform', 'translateY(-350px)');
-                $('#bottom').css('transform', 'translateY(-350px)');
-                $('#top').css('transform', 'translateY(-250px)');
-            } else {
-
-                if(!active) {
-                    console.log("Not true");
-                    $('#footer').css('transform', 'translateY(-300px)');
-                    active = true;
-                }
-                $('#main').css('opacity', '0');
-                $('#bottom').css('transform', 'translateY(-300px)');
-                $('#top').css('transform', 'translateY(-200px)');
-
+            if(active) {
+                $('#footer').slideDown();
             }
+
+            if(!active) {
+                console.log("Not true");
+                //$('#footer').css('transform', 'translateY(-300px)');
+                  active = true;
+                }
+            $('#main').css('opacity', '0');
+            $('#bottom').css('transform', 'translateY(-300px)');
+            $('#top').css('transform', 'translateY(-200px)');
+
         }else {
             console.log("Scroll Down");
+
+            //$('#bottom').css('transition-delay', '');
+            //$('#top').css('transition-delay', '');
+            $('#footer').slideUp();
             $('#main').css('opacity', '1');
             $('#bottom').css("transform", 'translateY(0)');
             $('#top').css('transform', 'translateY(0)');
-            $('#footer').css('transform', 'translateY(0)');
+            //$('#footer').css('transform', 'translateY(0)');
             active = false;
         }
     });
