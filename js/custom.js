e9ec235a90123666db2fa02cfc2ab5bb057708b1
williegroovy <williegroovy@yahoo.com>
2015-12-28 15:19:28 -0600

    Smoothed out the footer transition. Added navbar and logo (needs to be changed) Added collapse fa-bar. Added footer text.

--- js/custom.js
+++ js/custom.js
@@ -4,35 +4,27 @@ $(document).ready(function() {
     var active = false;
 
     $(window).scroll(function() {
-        console.log("Scroll: " + $window.scrollTop());
-        console.log("Window Size: " + $window.height());
-        if($window.scrollTop() > 0) {
-            console.log(active);
+        if($window.scrollTop() > 5) {
+            $('.fade-out').css('opacity', '0');
+            $('#bottom').css('transform', 'translateY(-300px)');
+            $('#top').css('transform', 'translateY(-200px)');
+
             if(active) {
                 $('#footer').slideDown();
+                $('#bottom').css('transform', 'translateY(-300px)');
+                $('#top').css('transform', 'translateY(-200px)');
                 $('#extra').html("");
             }
 
             if(!active) {
-                console.log("Not true");
-                $('#extra').html("<p>test</p>");
-                //$('#footer').css('transform', 'translateY(-300px)');
+                $('#extra').html("<p>' '</p>");
                   active = true;
                 }
-            $('#main').css('opacity', '0');
-            $('#bottom').css('transform', 'translateY(-300px)');
-            $('#top').css('transform', 'translateY(-200px)');
-
         }else {
-            console.log("Scroll Down");
-
-            //$('#bottom').css('transition-delay', '');
-            //$('#top').css('transition-delay', '');
             $('#footer').slideUp();
-            $('#main').css('opacity', '1');
+            $('.fade-out').css('opacity', '1');
             $('#bottom').css("transform", 'translateY(0)');
             $('#top').css('transform', 'translateY(0)');
-            //$('#footer').css('transform', 'translateY(0)');
             active = false;
         }
     });
