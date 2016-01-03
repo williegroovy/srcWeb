9b400fe49959d3cabc76062ad54b6f2718a45840
William <williegroovy@yahoo.com>
2016-01-03 12:44:45 -0600

    Added touch detection for menu and bottom/footer scrolling.

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,79 @@ function insideClose() {
 
 $(document).ready(function() {
     var del = 0;
+    var lastY;
     var menuOpen = false;
+
+    var ts;
+    $(document).bind('touchstart', function (e){
+
+        ts = e.originalEvent.touches[0].clientY;
+        console.log(ts);
+
+        if(ts < 100) {
+            $('#main-content').removeClass('disable-scrolling');
+        }
+
+        var isTouchMoveAllowed = true
+        var target = e.target;
+
+        while(target != null) {
+
+            if ( target.classList && target.classList.contains( 'disable-scrolling' ) ) {
+                isTouchMoveAllowed = false;
+                break;
+            }
+            target = target.parentNode;
+        }
+
+        if ( !isTouchMoveAllowed ) {
+            event.preventDefault();
+        }
+    });
+
+    $(document).bind('touchend', function (e){
+
+        var te = e.originalEvent.changedTouches[0].clientY;
+
+        if(ts > te+5){
+            touchMove(-1);
+        }else if(ts < te-5){
+            touchMove(1);
+        }
+
+        $('#main-content').addClass('disable-scrolling');
+    });
+
+    function touchMove(delta) {
+
+        del = del + delta;
+
+        if(del < -2){del = -2;}
+        if(del > 0){del = 0;}
+
+        console.log(del);
+        if(del <= -1) {
+            if(del == -2) {
+                $('#footer').css('transform', 'translateY(-470px)').css('-webkit-transform', 'translateY(-470px)');;
+                $('#bottom').css('transform', 'translateY(-462px)').css('-webkit-transform', 'translateY(-462px)');
+                $('#top').css('transform', 'translateY(-262px)').css('-webkit-transform', 'translateY(-262px)');
+
+            } else {
+                $('#footer').css('transform', 'translateY(-400px)').css('-webkit-transform', 'translateY(-400px)');
+                $('#bottom').css('transform', 'translateY(-400px)').css('-webkit-transform', 'translateY(-400px)');
+                $('#top').css('transform', 'translateY(-200px)').css('-webkit-transform', 'translateY(-200px)');
+            }
+        } else {
+            if(del == -1) {
+                $('#footer').css('transform', 'translateY(0px)').css('-webkit-transform', 'translateY(-400px)');
+                $('#top').css('transform', 'translateY(-200px)').css('-webkit-transform', 'translateY(-200px)');
+                $('#bottom').css('transform', 'translateY(-400px)').css('-webkit-transform', 'translateY(-400px)');
+            }else {
+                $('#bottom').css('transform', 'translateY(0px)').css('-webkit-transform', 'translateY(0px)');
+                $('#top').css('transform', 'translateY(0px)').css('-webkit-transform', 'translateY(0px)');
+            }
+        }
+    }
 
     var mainContent = document.getElementById('main-content');
 
@@ -19,19 +91,17 @@ $(document).ready(function() {
     $('#close-btn').on('click', btnClicked);
     $('#btn').on('click', btnClicked);
 
-
-
     function btnClicked() {
         if(!menuOpen) {
             menuOpen = true;
             $('#menu').removeClass('fa-bars').addClass('fa-angle-double-right');
-            $('#right').css('transform', 'translate3d(100vw, 0, 0) translateX(-360px)').css('-webkit-transform', 'translate3d(100vw, 0, 0) translateX(-360px)');
+            $('#right').css('display', 'block').css('transform', 'translate3d(100vw, 0, 0) translateX(-360px)').css('-webkit-transform', 'translate3d(100vw, 0, 0) translateX(-360px)');
             $('#top').css('transform', 'translateX(-360px)').css('-webkit-transform', 'translateX(-360px)');
         } else {
             menuOpen = false;
             $('#menu').removeClass('fa-angle-double-right').addClass('fa-bars');
             $('#right').css('transform', 'translate3d(100vw, 0, 0) translateX(0px)').css('-webkit-transform', 'translate3d(100vw, 0, 0) translateX(0px)');
-            $('#top').css('transform', 'translateX(-0px)').css('-webkit-transform', 'translateX(-0px)');
+            $('#top').css('transform', 'translateX(0px)').css('-webkit-transform', 'translateX(0px)');
         }
     }
 
@@ -58,7 +128,7 @@ $(document).ready(function() {
             }
         } else {
             if(del == -1) {
-                $('#footer').css('transform', 'translateY(0px)').css('-webkit-transform', 'translateY(0px)');
+                $('#footer').css('transform', 'translateY(0px)').css('-webkit-transform', 'translateY(-4000px)');
                 $('#top').css('transform', 'translateY(-200px)').css('-webkit-transform', 'translateY(-200px)');
                 $('#bottom').css('transform', 'translateY(-400px)').css('-webkit-transform', 'translateY(-400px)');
             }else {
